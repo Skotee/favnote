61550c4bc8760b33f142c78cd5c76690773566b0
Radek <radoslaw.wojaczek@itsilesia.com>
2020-11-05 15:22:31 +0100

    configure storybook

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,38 @@
+// src/components/Button.stories.js
+
+import React from 'react';
+
+import Button from './Button';
+
+export default {
+  component: Button,
+  title: 'Button',
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: '1',
+    title: 'Test Task',
+    state: 'TASK_INBOX',
+    updatedAt: new Date(2018, 0, 1, 9, 0),
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_PINNED',
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_ARCHIVED',
+  },
+};
